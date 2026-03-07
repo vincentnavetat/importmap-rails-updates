9ad0070835c4fe43373cff04e490d79355822030
Vincent Navetat <vincent.navetat@carwow.co.uk>
2026-03-07 22:38:15 +0000

    Add outdated packages in importmaps config

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "react", to: "https://ga.jspm.io/npm:react@17.0.2/index.js"
+pin "idiomorph", to: "https://ga.jspm.io/npm:idiomorph@0.4.0/dist/idiomorph.esm.js"
+pin "swiper", to: "https://ga.jspm.io/npm:swiper@12.0.0/swiper.mjs"
