622572a5cd593309a0caed811d427e513415ea46
Vincent Navetat <vincent.navetat@carwow.co.uk>
2026-03-08 21:07:18 +0000

    Update react to 19.2.4

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "react", to: "https://ga.jspm.io/npm:react@17.0.2/index.js"
+pin "react" # @19.2.4
 pin "idiomorph", to: "https://ga.jspm.io/npm:idiomorph@0.4.0/dist/idiomorph.esm.js"
 pin "swiper", to: "https://ga.jspm.io/npm:swiper@12.0.0/swiper.mjs"
