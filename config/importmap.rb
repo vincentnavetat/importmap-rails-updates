c90c06e08a873109ff8cc644157c3abf9d57a0b5
Vincent Navetat <vincent.navetat@carwow.co.uk>
2026-03-09 21:49:54 +0000

    Bump react from 17.0.2 to 19.2.4

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "react", to: "https://ga.jspm.io/npm:react@17.0.2/index.js"
+pin "react" # @19.2.4
 pin "idiomorph" # @0.4.0
